e9f1c96eba68431fdd1b37b59ec3b2bf669ec152
Sunny Ripert <sunny@sunfox.org>
2020-06-14 12:50:28 +0200

    Call the Git default branch "main"

--- install.sh
+++ install.sh
@@ -8,21 +8,37 @@ link() {
   [[ ! $to ]] && to="$HOME/$1"
 
   if [[ ! -f $to ]]; then
-    echo $to
+    echo "-----> Linking $to"
     mkdir -p "`dirname $to`"
     ln -s $from $to
   fi
 }
 
-# zsh plugins
-CURRENT_DIR=`pwd`
-ZSH_PLUGINS_DIR="$HOME/.oh-my-zsh/custom/plugins"
-mkdir -p "$ZSH_PLUGINS_DIR" && cd "$ZSH_PLUGINS_DIR"
-if [ ! -d "$ZSH_PLUGINS_DIR/zsh-syntax-highlighting" ]; then
-  echo "-----> Installing zsh plugin 'zsh-syntax-highlighting'..."
-  git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
-fi
-cd "$CURRENT_DIR"
+copy() {
+  from="$PWD/copies/$1"
+  to="$2"
+  [[ ! $to ]] && to="$HOME/$1"
+
+  if [[ ! -f $to ]]; then
+    echo "-----> Creating $to"
+    mkdir -p "`dirname $to`"
+    cp $from $to
+  fi
+}
+
+install_zsh_plugin() {
+  CURRENT_DIR=`pwd`
+  ZSH_PLUGINS_DIR="$HOME/.oh-my-zsh/custom/plugins"
+  mkdir -p "$ZSH_PLUGINS_DIR" && cd "$ZSH_PLUGINS_DIR"
+  if [ ! -d "$ZSH_PLUGINS_DIR/zsh-syntax-highlighting" ]; then
+    echo "-----> Installing zsh plugin 'zsh-syntax-highlighting'..."
+    git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
+  fi
+  cd "$CURRENT_DIR"
+}
+
+# Plugins
+install_zsh_plugin
 
 # Links
 link .aliases
@@ -40,5 +56,6 @@ link bin/gh-rename-master
 link .psql/.keep
 link vscode-settings.json "$HOME/Library/Application Support/Code/User/settings.json"
 
-# Local file
-touch "$HOME/.zshlocal"
+# Copies
+copy .config/git/template/HEAD
+copy .zshlocal
