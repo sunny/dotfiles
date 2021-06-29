531e8e5695e0400342d966419d3e87238914f8c2
Sunny Ripert <sunny@sunfox.org>
2021-06-29 10:44:05 +0200

    …

--- install.sh
+++ install.sh
@@ -31,7 +31,7 @@ install_zsh_plugin() {
   ZSH_PLUGINS_DIR="$HOME/.oh-my-zsh/custom/plugins"
   mkdir -p "$ZSH_PLUGINS_DIR" && cd "$ZSH_PLUGINS_DIR"
   if [ ! -d "$ZSH_PLUGINS_DIR/zsh-syntax-highlighting" ]; then
-    echo "-----> Installing zsh plugin 'zsh-syntax-highlighting'..."
+    echo "-----> Installing zsh plugin 'zsh-syntax-highlighting'…"
     git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
   fi
   cd "$CURRENT_DIR"
