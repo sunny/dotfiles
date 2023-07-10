b1228e4d8618af85fc6d61a51d94865f32454fcb
Sunny Ripert <sunny@sunfox.org>
2023-07-10 20:16:38 +0200

    Use ssh syntax for zsh syntax highlighting plugin

--- install.sh
+++ install.sh
@@ -32,7 +32,7 @@ install_zsh_plugin() {
   mkdir -p "$ZSH_PLUGINS_DIR" && cd "$ZSH_PLUGINS_DIR"
   if [ ! -d "$ZSH_PLUGINS_DIR/zsh-syntax-highlighting" ]; then
     echo "-----> Installing zsh plugin 'zsh-syntax-highlighting'…"
-    git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
+    git clone git@github.com:zsh-users/zsh-syntax-highlighting.git
   fi
   cd "$CURRENT_DIR"
 }
